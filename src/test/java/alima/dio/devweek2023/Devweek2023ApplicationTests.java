1a94db647398ad16422d9d443d700bbae4bb62ff
limaand <limaand@gmail.com>
2023-10-17 11:57:18 -0300

    inicial

---
+++ src/test/java/alima/dio/devweek2023/Devweek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package alima.dio.devweek2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devweek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
